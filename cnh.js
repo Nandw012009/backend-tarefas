1c37fa02362bbd5735565a60e5ebb98d03fc57af
Nandw012009 <oliveira.pereira.fernanda@escola.pr.gov.br>
2026-03-30 13:50:38 +0000

    versão2

--- cnh.js
+++ cnh.js
@@ -1,4 +1,12 @@
-const idade = 15;
-const maioridade = idade >= 18;
+const idade = 16;
+const maiorIdade = idade >= 18;
 const possuiCNH = false;
 
+const podeDirigir = maiorIdade && possuiCNH;
+console.log(`Pode viajar sozinho?`, podeDirigir);
+
+const podeViajarSozinho = maiorIdade || possuiCNH 
+console.log(`Pode viajar sozinho?`, podeViajarSozinho);
+
+const precisaDeAcompanhante = !maiorIdade;
+console.log(`Precisa de Acompanhante?`, precisaDeAcompanhante);
