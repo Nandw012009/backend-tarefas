8ef98b1c35b1c57185f72f9285018e1aec543282
Nandw012009 <oliveira.pereira.fernanda@escola.pr.gov.br>
2026-04-29 12:19:29 +0000

    versao2

--- objeto.js
+++ objeto.js
@@ -9,3 +9,16 @@ console.log("Material escolhido: ",  bancoAutomotivo.material);
 
 bancoAutomotivo.concluido = true;
 console.log(bancoAutomotivo);
+
+
+
+const papelaria = {
+    caneta: "vermelha",
+    papel: "colorido", 
+    lapis: "12 cores",
+    caderno:"48 paginas",
+    mochila: "preto",
+
+    console.log("O material escolhido da papelaria, é uma caixa de lápis de", papelaria.lapis);
+    
+}
